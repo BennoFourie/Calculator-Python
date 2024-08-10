e5589ca4e4be528578db67a266b8ab2a8ed2e897
BennoFourie <bennofourie2006@gmail.com>
2024-08-10 20:00:04 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+while True:
+    number1 =int(input("enter first number: "))
+    number2 =int(input("enter second number: "))
+    x = input("enter +, -, * or /: ")
+
+    if x == "+":
+      print(number1 + number2)
+      
+    elif x == "-":
+      print(number1 - number2)
+      
+    elif x == "*":
+      print(number1 * number2)
+      
+    elif x == "/":
+      print(number1 / number2)
+
+    
